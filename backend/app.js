be7f22f0089aaad9dc7fff46547db6818970fce2
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-01-13 23:13:04 +0500

    main

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,13 @@
 const express = require("express")
-const app =  express()
+const app = express()
 require("dotenv").config()
 //initalizing Cloud Functions (firebase.)
-const {initializeApp}=require("firebase-admin/app")
-initializeApp()
-const {getFirestore,Timestamp,FieldValue,Filter}=require("firebase-admin/firestore")
-const db=getFirestore()
-
-
+const {initializeApp,applicationDefault,cert} =require("firebase-admin/app")
+const serviceAccount=require("./db/Firebase.json")
+const admin=require("firebase-admin")
+initializeApp({
+    credential:admin.credential.cert(serviceAccount),
+    databaseURL:process.env.DATABASE_URL
+})
 
 module.exports = { app }

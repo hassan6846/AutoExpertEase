12215836817894c41a1353ac58441527a0f84c9b
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-01-23 02:08:36 +0500

    added loginPage

--- app/App.tsx
+++ app/App.tsx
@@ -7,19 +7,49 @@
 //navigator (react navigation)
 import { NavigationContainer } from "@react-navigation/native"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
+
 const Stack = createNativeStackNavigator()
 //pages or Screens
+import LoginPage from "./screens/LoginScreen"
+import { TouchableOpacity, View } from "react-native"
+import { Icon } from "@rneui/themed"
+
+//props
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
         {/* EnrollScreen */}
-        <Stack.Screen name="" component={ } />
+        <Stack.Screen name="login" component={LoginPage}
+          options={{
+            title: "Get Started",
+            headerShadowVisible: false,
+            headerTitleAlign: "center",
+            headerLeft: () => (
+              // Custom component for the back arrow
+              // You can use TouchableOpacity for better touch feedback
+              <TouchableOpacity activeOpacity={0.7}
+                style={{ height: "auto",borderRadius:50,shadowColor:"#97ADB6"}}
+              >
+                <Icon
+                  size={18}
+                  iconStyle={{fontSize:30}}
+                  name="chevron-left"
+                  type="evilicon"
+                 
+                  raised={true}
+                />
+                {/* Use the appropriate icon library */}
+              </TouchableOpacity>
+            ),
+          }}
+
+        />
         {/* LoginScreen Phone Number */}
-        <Stack.Screen name="" component={ } />
+        {/* <Stack.Screen name="" component={ } /> */}
         {/* Otp Screen */}
-        <Stack.Screen name="" component={ } />
+        {/* <Stack.Screen name="" component={ } /> */}
       </Stack.Navigator>
     </NavigationContainer>
   );
--- app/screens/LoginScreen.tsx
+++ app/screens/LoginScreen.tsx
@@ -1,2 +1,81 @@
-import {Input} from "@rneui/themed"
-import {StatusBar} from "expo-status-bar"
+import { View, StyleSheet, KeyboardAvoidingView, Platform, ScrollView, Image, TouchableOpacity } from "react-native"
+import { useRef, useState, useEffect, createRef } from "react"
+
+import { StatusBar } from "expo-status-bar"
+import { Button, Icon, Input, Text } from "@rneui/themed"
+//Page
+const LoginPage = () => {
+    const InputRef = createRef()
+    const [Phone, SetPhone] = useState("")
+    //InputErrors
+
+    return (
+        <KeyboardAvoidingView
+            style={{ flex: 1 }}
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+        >
+            <ScrollView contentContainerStyle={styles.scrollContainer}>
+                <View style={styles.LoginContainer}>
+                    <StatusBar />
+                    <Text style={styles.LoginHeading} h3={true} >Enter Phone Number for Verification</Text>
+                    <Text style={styles.LoginSub}>This number will be used for all ride-related communication. You shall receive an SMS with code for verification.</Text>
+                    <Input inputContainerStyle={styles.LoginInputCont} rightIcon={<TouchableOpacity activeOpacity={0.7} ><Icon name="close" reverseColor="#66696D" reverse={true} color="#e3e3e3" iconStyle={{ fontSize: 15, fontWeight: "bold" }} type="evilicon" size={8} raised={true} /></TouchableOpacity>} leftIcon={<Image style={{ width: 35, height: 30, shadowColor: "red" }} source={require('../assets/pakflag.png')} />} placeholder="+92 399 3148142" errorMessage="Sorry! Rate Limit Exceded Please Try Later in 1hr." keyboardType="number-pad" />
+                    <Button buttonStyle={styles.LoginButton} color="#E04E2F" title="Send Otp" />
+                    <Text style={styles.LoginConsent}>By providing my phone number, I hereby agree and accept the <Text style={styles.LoginLink}>Terms of Service</Text> and <Text style={styles.LoginLink}>Privacy Policy</Text> in use of the  app.</Text>
+                </View>
+            </ScrollView>
+        </KeyboardAvoidingView>
+    );
+}
+
+//styles
+const styles = StyleSheet.create({
+    LoginContainer: {
+        padding: 30,
+        flex: 1,
+        backgroundColor: "#fff"
+    },
+    // ScrollView
+    scrollContainer: {
+        flexGrow: 1,
+    },
+    LoginHeading: {
+        marginTop: 10,
+        textAlign: 'left',
+        marginBottom: 14,
+    },
+    LoginSub: {
+        marginBottom: 10,
+        color: "#97ADB6",
+        textAlign: 'left'
+    },
+    LoginInputCont: {
+        width: "100%",
+        borderRadius: 5,
+        marginBottom: 2,
+        padding: 5,
+        borderTopWidth: 0.5,
+        borderLeftWidth: 0.5,
+        borderRightWidth: 0.5,
+        borderBottomWidth: 0.5,
+    },
+    LoginButton: {
+        padding: 12,
+        borderRadius: 10,
+
+    },
+
+    LoginLink: {
+        textDecorationLine: "underline",
+        fontWeight: "500",
+        color: "#E04E2F"
+    },
+    LoginConsent: {
+        marginTop: 10,
+        color: "#97ADB6",
+        padding: 5,
+        textAlign: "center"
+    }
+
+})
+export default LoginPage;

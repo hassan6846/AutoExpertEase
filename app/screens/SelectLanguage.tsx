78cfbc9ab5d104d23b4fc47a678aa45ac6fae426
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-01-26 01:54:45 +0500

    selectLanguage added

--- app/screens/SelectLanguage.tsx
+++ app/screens/SelectLanguage.tsx
@@ -1,17 +1,31 @@
 //modules
-import { SafeAreaView, ScrollView, View,StyleSheet } from "react-native"
-import { } from "@rneui/themed"
+import { SafeAreaView, ScrollView, View, StyleSheet } from "react-native"
+import { Text, Input, Button } from "@rneui/themed"
 //utils
 
 //main void Function
 const SelectLanguage = ({ navigation }: { navigation: any }) => {
   return (
-    <SafeAreaView></SafeAreaView>
-    )
+    <View>
+      <Text style={styles.SelectLangHead} h3={true}>Choose The Language</Text>
+      <Input placeholder="Search Desired Language" />
+      <SafeAreaView>
+        <ScrollView></ScrollView>
+      </SafeAreaView>
+      <Button buttonStyle={styles.LanguageSelectBTn} color="#E04E2F" title="Select" />
+    </View>
+  )
 }
 
 // Styles
-
+const styles = StyleSheet.create({
+  SelectLangHead: {
+    textAlign: "center"
+  }, LanguageSelectBTn: {
+    padding: 12,
+    borderRadius: 10,
+  }
+})
 
 // exported
 export default SelectLanguage

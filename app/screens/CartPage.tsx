b952235859a930f20ecdf20608734a28aca82ef4
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-01-26 03:59:08 +0500

    main

---
+++ app/screens/CartPage.tsx
@@ -0,0 +1,13 @@
+//modules
+import {View,} from "react-native"
+//utils or props
+//statics
+
+
+const CartPage = () => {
+    return (
+        <View>CartPage</View>
+    )
+}
+
+export default CartPage
---
+++ app/screens/SetupAccountType.tsx
@@ -0,0 +1,10 @@
+import {View} from "react-native";
+import React from 'react'
+
+const SetupAccountType = () => {
+  return (
+    <View>SetupAccountType</View>
+  )
+}
+
+export default SetupAccountType

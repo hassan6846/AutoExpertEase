600895d1d74a4712a1ee4b53b0694498717f107a
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-02-01 14:14:18 +0500

    profiletab customized

--- app/screens/ProfileTab.tsx
+++ app/screens/ProfileTab.tsx
@@ -1,11 +1,28 @@
-import { ScrollView, StyleSheet, View } from "react-native"
+import { ScrollView, StyleSheet, View, TouchableOpacity } from "react-native"
 import { Text, ListItem, Icon, Badge, Avatar } from "@rneui/themed"
-import {} from "@gorhom/bottom-sheet"
+import CountryFlag from "react-native-country-flag"
+import { } from "@gorhom/bottom-sheet"
+
+import { AvatarSrc, DefaultImageSrc } from "../utils/ImageUrls"
+
 
 const ProfileTab = () => {
     return (
         <ScrollView style={Styles.ProfileSettingContainer}>
-        
+            {/* Avatar container bois to alignItems to the center */}
+            <View style={Styles.AvatarContainer} >
+                <Avatar rounded size={100} source={{ uri: AvatarSrc || DefaultImageSrc }} />
+                <Text>Hi, {"user"} <CountryFlag size={12} isoCode="pk" /></Text>
+            </View>
+            {/* List Items Will be map Below ok */}
+            <Text style={{ marginTop: 8, marginBottom: 7, marginLeft: 10 }}>Account</Text>
+            <ListItem containerStyle={Styles.ProfileListStyle} >
+                <Icon name="person" type="material" color="grey" />
+                <ListItem.Content>
+                    <ListItem.Title>Switch Role:</ListItem.Title>
+                </ListItem.Content>
+                <ListItem.Chevron />
+            </ListItem>
         </ScrollView>
     )
 }
@@ -14,10 +31,24 @@ const ProfileTab = () => {
 
 const Styles = StyleSheet.create({
     ProfileSettingContainer: {
-    
-        backgroundColor: "red",
         flex: 1,
-        padding: 30,
+        padding: 25,
+
+    },
+    AvatarContainer: {
+
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: "center",
+        padding: 10,
+        rowGap: 5,
+        justifyContent: "center"
+    },
+    // List items Style styles will only be applied through the ContainerStyleProp.
+
+    ProfileListStyle: {
+        shadowColor: "#000000",
+        borderRadius: 5,
     }
 })
 export default ProfileTab

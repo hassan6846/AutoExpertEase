0f56faf18ab79b1c6a7935e45f5132b241101341
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-02-02 22:58:32 +0500

    main

--- app/screens/BottomsTabs/HomeTab.tsx
+++ app/screens/BottomsTabs/HomeTab.tsx
@@ -1,11 +1,13 @@
-import { SafeAreaView,StyleSheet, Text } from "react-native"
+import { SafeAreaView, StyleSheet, Text } from "react-native"
+
 const Home = () => {
+
   return (
     <SafeAreaView ><Text>Home</Text></SafeAreaView>
   )
 }
 // styleSheet
-const Styles=StyleSheet.create({
+const Styles = StyleSheet.create({
 
 })
 export default Home

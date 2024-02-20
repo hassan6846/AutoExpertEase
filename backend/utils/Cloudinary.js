905fb80d15a5947edd4f7ae4b3ca42d2d4b43882
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-02-20 07:55:30 +0500

    update backend

--- backend/utils/Cloudinary.js
+++
@@ -1,9 +0,0 @@
-const cloudinary =require("cloudinary").v2
-cloudinary.config({ 
-  cloud_name: process.env.CLOUD_NAME, 
-  api_key: process.env.API_KEY, 
-  api_secret: process.env.API_SECRET 
-});
-//for Naming Convention the Name Alias is name Different for Module Export ..
-//but instances and method would remain same..
-module.exports={cloudinaryInstance:cloudinary}

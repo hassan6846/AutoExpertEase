f74bb625a34b560834f6f4cfc59b917b652cbbe6
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-03-02 07:10:48 +0500

    Update ShopCategory.tsx

--- app/screens/Stacks/Shop/nested/ShopCategory.tsx
+++ app/screens/Stacks/Shop/nested/ShopCategory.tsx
@@ -1,46 +1,95 @@
-import { View, FlatList, StyleSheet, } from "react-native"
-import { Text,Avatar } from "@rneui/themed"
+import React from 'react';
+import { View, FlatList, StyleSheet } from 'react-native';
+import { Text, Avatar } from '@rneui/themed';
+import { AvatarSrc } from '../../../../constants/ImagesConstants';
 
 const ShopCategory = ({ navigation }: { navigation: any }) => {
-    // CateogoryData
+    // CategoryData
     const CategoryApi = [
         // Interior Collection
         {
-            Title: "Interior Collection",
-            Items:[
+            Title: 'Interior Collection',
+            Items: [
                 {
-                    Image:"",
-                    Title:"Stearing Wheel"
+                    Image:
+                        'https://res.cloudinary.com/diml3oeaw/image/upload/v1709258667/AutoExpertEase/Assets/ShopCateogryApi_Images/Interior/hxrsdy4knmgbuzkyhfgv.png',
+                    title: 'Steering Wheel',
                 },
-            ]
+                {
+                    Image:
+                        'https://res.cloudinary.com/diml3oeaw/image/upload/v1709258667/AutoExpertEase/Assets/ShopCateogryApi_Images/Interior/wj5viu4cfdqiseszzecl.png',
+                    title: 'Seating & Furniture.',
+                },
+                {
+                    Image:
+                        'https://res.cloudinary.com/diml3oeaw/image/upload/v1709258667/AutoExpertEase/Assets/ShopCateogryApi_Images/Interior/okkapuadloin5grwt73l.png',
+                    title: 'Ash Tray',
+                },
+                {
+                    Image:
+                        'https://res.cloudinary.com/diml3oeaw/image/upload/v1709258667/AutoExpertEase/Assets/ShopCateogryApi_Images/Interior/wj5viu4cfdqiseszzecl.png',
+                    title: 'Floor Mats',
+                },
+                {
+                    Image:
+                        'https://res.cloudinary.com/diml3oeaw/image/upload/v1709258666/AutoExpertEase/Assets/ShopCateogryApi_Images/Interior/fkminxuekzhsfhdft1mk.png',
+                    title: 'Automotive Shades',
+                },
+                {
+                    Image:
+                        'https://res.cloudinary.com/diml3oeaw/image/upload/v1709258667/AutoExpertEase/Assets/ShopCateogryApi_Images/Interior/o4rzekp4iuyn21arbcub.png',
+                    title: 'Speakers',
+                },
+                {
+                    Image:
+                        'https://res.cloudinary.com/diml3oeaw/image/upload/v1709258667/AutoExpertEase/Assets/ShopCateogryApi_Images/Interior/gtkvs3nyspdlwmigpkr7.png',
+                    title: 'Hangings',
+                },
+
+            ],
         },
-        //Exterior Collection.
+        // Exterior Collection.
         {
-            Title: "Exterior Collection",
-            Items:[]
+            Title: 'Exterior Collection',
+            Items: [],
         },
+    ];
 
-
-    ]
     return (
-
         <FlatList
             style={Styles.CategoryContainer}
             data={CategoryApi}
-            renderItem={({item})=>(
+            renderItem={({ item }) => (
                 <>
-                <Text h4>{item.Title}</Text>
-          
+                    <Text h4>{item.Title}</Text>
+                    <View style={Styles.AvatarsContainers}>
+                        <View style={Styles.AvatarStartFlow}>
+                            <Avatar source={{uri:AvatarSrc}}/>
+                        </View>
+                    </View>
+
                 </>
             )}
+            keyExtractor={(item) => item.Title}
         />
+    );
+};
 
-    )
-}
 // Styles
 const Styles = StyleSheet.create({
     CategoryContainer: {
-        flex: 1
+        flex: 1,
+    },
+    AvatarsContainers: {
+        flex: 1,
+        flexDirection: "row",
+        backgroundColor: "red",
+        justifyContent:"center"
+    },AvatarStartFlow:{
+        flexDirection:"row",
+        justifyContent:"flex-start"
     }
-})
-export default ShopCategory
+});
+
+
+export default ShopCategory;

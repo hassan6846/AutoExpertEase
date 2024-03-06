9fe32236a8cdb8e349be81cf67999e1e835d4ee7
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-03-06 13:48:32 +0500

    Update CartSlice.tsx

--- app/slices/CartSlice.tsx
+++ app/slices/CartSlice.tsx
@@ -1,52 +1,16 @@
-import {createSlice} from "@reduxjs/toolkit"
-
+import { createSlice } from "@reduxjs/toolkit";
 
 const cartSlice = createSlice({
-  name: 'cart',
-  initialState: {
-    items: []
-  },
-  reducers: {
-    addItem: (state, action) => {
-      const newItem = action.payload;
-      const existingItem = state.items.find(item => item.id === newItem.id);
-
-      if (existingItem) {
-        existingItem.quantity += 1;
-      } else {
-        state.items.push({ ...newItem, quantity: 1 });
-      }
+    name: 'cart',
+    initialState: {
+        items: [],
+        total: 0,
     },
-    removeItem: (state, action) => {
-      const itemId = action.payload;
-      state.items = state.items.filter(item => item.id !== itemId);
-    },
-    incrementQuantity: (state, action) => {
-      const itemId = action.payload;
-      const itemToIncrement = state.items.find(item => item.id === itemId);
+    //   
+    reducers: {
       
-      if (itemToIncrement) {
-        itemToIncrement.quantity += 1;
-      }
-    },
-    decrementQuantity: (state, action) => {
-      const itemId = action.payload;
-      const itemToDecrement = state.items.find(item => item.id === itemId);
-      
-      if (itemToDecrement) {
-        itemToDecrement.quantity -= 1;
-
-        if (itemToDecrement.quantity === 0) {
-          // Optionally remove the item if quantity becomes zero
-          state.items = state.items.filter(item => item.id !== itemId);
-        }
-      }
-    },
-  },
+    }
 });
 
 export const { addItem, removeItem, incrementQuantity, decrementQuantity } = cartSlice.actions;
 export default cartSlice.reducer;
-
-
-

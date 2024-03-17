3b367bca52198ffc8c61eb45f380a6dc2c44eb69
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-03-18 02:34:16 +0500

    Update ChatSupport.tsx

--- app/screens/Stacks/UserProfile/-nested/Help&Support/ChatSupport.tsx
+++ app/screens/Stacks/UserProfile/-nested/Help&Support/ChatSupport.tsx
@@ -1,13 +1,40 @@
-import { View, Text } from 'react-native'
-import { } from "react-native-gifted-chat"
-import React from 'react'
+import React, { useState, useCallback, useEffect } from 'react';
+import { GiftedChat, IMessage, User } from 'react-native-gifted-chat';
 
-const ChatSupport = () => {
+interface ChatSupportProps {}
+
+ const ChatSupport: React.FC<ChatSupportProps> = () => {
+  const [messages, setMessages] = useState<IMessage[]>([]);
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+    ]);
+  }, []);
+
+  const onSend = useCallback((newMessages: IMessage[] = []) => {
+    setMessages(previousMessages =>
+      GiftedChat.append(previousMessages, newMessages),
+    );
+  }, []);
+
   return (
-    <View>
-      <Text>ChatSupport</Text>
-    </View>
-  )
-}
-
+    <GiftedChat
+      messages={messages}
+      onSend={newMessages => onSend(newMessages)}
+      user={{
+        _id: 1,
+      }}
+    />
+  );
+};
 export default ChatSupport

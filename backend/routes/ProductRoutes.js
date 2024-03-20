d1a34e5a9089c25360d35bf679f3d11404037f2a
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-03-20 08:07:21 +0500

    Update ProductRoutes.js

--- backend/routes/ProductRoutes.js
+++ backend/routes/ProductRoutes.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const { CreateProductListing } = require("../controllers/ProductControllers")
+router.route("/product")//get all products
+router.route("/product/:id")//get a single product by id 
+router.route("/product/search")//search for product
+router.route("/product/nearby")// find product from nearby sellers...
+router.route("/product/update/:id")//update a single product by id
+router.route("/product/delete/:id")//delete a single product by id
 router.route('/product/create',CreateProductListing)
 
 // exporting all routes.

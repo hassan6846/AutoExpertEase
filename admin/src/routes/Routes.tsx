a027b5e9a3b51cbb60029a1c079faaeff8819322
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-03-22 09:35:16 +0500

    main

--- admin/src/routes/Routes.tsx
+++ admin/src/routes/Routes.tsx
@@ -1,1 +1,21 @@
-import {createBrowserRouter} from "react-router-dom"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+
+
+
+//pages
+
+
+import ErrorPage from "../pages/404/ErrorPage"
+import React from "react"
+
+
+const Routing = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<ErrorPage />} />
+            </Routes>
+        </Router>
+    )
+}
+export default Routing

984e1a230cd037071df8cf550c5968dec9c5cc42
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-03-29 10:50:35 +0500

    Update ViewVideos.tsx

--- app/screens/Stacks/Home/nested/ViewVideos.tsx
+++ app/screens/Stacks/Home/nested/ViewVideos.tsx
@@ -1,17 +1,17 @@
-import { View,  } from 'react-native'
+import { View,ScrollView } from 'react-native'
 import React from 'react'
 //library
 import {Text} from "@rneui/themed"
+import {} from "@gorhom/bottom-sheet"
 //sdks
 import {Video} from "expo-av"
 const ViewVideos = () => {
   return (
     <View>
      <Video 
-
+     
      useNativeControls={true}
     shouldPlay={true}
-
      style={{height:300,width:"100%"}}
      source={{uri:"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4"}}/>
     </View>

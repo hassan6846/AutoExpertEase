dcba4534c2fe7c60f826bbd4018adab7ed536347
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-03-29 11:12:19 +0500

    Create VendorSucess.tsx

---
+++ app/screens/VendorSignup/VendorSucess.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const VendorSucess = () => {
+  return (
+    <View>
+      <Text>VendorSucess</Text>
+    </View>
+  )
+}
+
+export default VendorSucess

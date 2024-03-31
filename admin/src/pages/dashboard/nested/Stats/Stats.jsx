9c3b9ea416d16b14bf7fb9d94ffab3a0bb61b56e
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-03-31 23:58:28 +0500

    Update Stats.jsx

--- admin/src/pages/dashboard/nested/Stats/Stats.jsx
+++ admin/src/pages/dashboard/nested/Stats/Stats.jsx
@@ -4,6 +4,8 @@ const Stats = () => {
   return (
     <div style={{ flex: 1, backgroundColor: "#fff" }}>
       <Header title="Statistics"/>
+      {/* Cards for maping the earning daily users etc */}
+      <div></div>
     </div>
   )
 }

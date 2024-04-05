baf3d86ac651e2e6a990182b9b47bb18976a33ef
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-04-05 09:20:17 +0500

    added maptoken

--- build/App.js
+++ build/App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Mapbox from "@rnmapbox/maps"
+Mapbox.setAccessToken('pk.eyJ1IjoiaGFzc2Fuc2hlcml5YXIiLCJhIjoiY2xybnIxam00MTg0djJscXI1bXVxNTR3aCJ9.4r9apA2hHuxU3tOoGDVZbQ')
 export default function App() {
   return (
     <View style={styles.container}>

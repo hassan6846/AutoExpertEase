dfcaa9aeeb0d5d713f982ba4af567705c47f7143
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-05-21 05:16:31 +0500

    main

--- admin/src/pages/dashboard/nested/Oveview/Overview.jsx
+++ admin/src/pages/dashboard/nested/Oveview/Overview.jsx
@@ -4,7 +4,7 @@ import Header from '../../../../components/Header/Header'
 const Overview = () => {
   return (
     <div style={{ flex: 1, backgroundColor: "#fff" }}>
- <Header title="Hi,Admin"/>
+      <Header title="Hi,Admin" />
 
     </div>
   )
--- admin/src/pages/dashboard/nested/VendorRequest/VendorRequest.jsx
+++ admin/src/pages/dashboard/nested/VendorRequest/VendorRequest.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import Header from '../../../../components/Header/Header'
-
+//Mui
+import Avatar from "@mui/material/Avatar"
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import { defaultUserImg } from '../../../../constants/ImageConstants'
+//css
+import "./VendorRequest.css"
 const VendorRequest = () => {
   return (
     <div style={{ flex: 1, backgroundColor: "#fff" }}>
- <Header title="Vendor Requests"/>
+      <Header title="Vendor Requests" />
+      {/* Card Example map Container */}
+      <section className='RequestContainer'>
+        <div className='RequestCard'>
 
+          <div className='TextContainerRequests'>
+          <Avatar src={defaultUserImg} />
+          <div>  <p><span style={{ fontWeight: "bold", marginBottom: 0 }}>Hassan Shehriyar </span>applied for vendorShip on the app</p>
+            <p style={{ fontSize: '10px', marginTop: 0, marginBottom: 0 }}>2hrs Ago</p></div>
+          </div>
+          <NavigateNextIcon />
+        </div>
+      </section>
+      {/* Containe Ends */}
     </div>
   )
 }

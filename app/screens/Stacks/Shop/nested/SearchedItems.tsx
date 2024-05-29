7bab0642af46cf0125b10c0a9577a5f3bbffa047
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-05-29 19:26:00 +0500

    main

---
+++ app/screens/Stacks/Shop/nested/SearchedItems.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SearchedItems = () => {
+  return (
+    <View>
+      <Text>SearchedItems</Text>
+    </View>
+  )
+}
+
+export default SearchedItems

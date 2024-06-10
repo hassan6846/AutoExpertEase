61dcf2b3322fad3b83ce75067a49749fac1392e2
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-06-11 04:09:52 +0500

    main

---
+++ app/screens/ExpertVerification/nested/ExpertVerification/CarRentalVerification.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const CarRentalVerification = () => {
+  return (
+    <View>
+      <Text>CarRentalVerification</Text>
+    </View>
+  )
+}
+
+export default CarRentalVerification

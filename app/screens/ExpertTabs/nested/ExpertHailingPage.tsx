bb215abdd06f121d15ca8c29524d14b1931b488f
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-06-13 06:05:01 +0500

    added i18 for language

--- app/screens/ExpertTabs/nested/ExpertHailingPage.tsx
+++ app/screens/ExpertTabs/nested/ExpertHailingPage.tsx
@@ -1,48 +1,21 @@
 import { View, Text, StyleSheet, Alert } from 'react-native'
 import React from 'react'
-import {useFocusEffect, useNavigation} from "@react-navigation/native"
-import showAlert from '../../../utils/ShowAlert'
+import { useFocusEffect, useNavigation } from "@react-navigation/native"
 
-const ExpertHailingPage = ({navigation}:{navigation:any}) => {
+
+const ExpertHailingPage = ({ navigation }: { navigation: any }) => {
   const navigator = useNavigation()
-  useFocusEffect(
-    React.useCallback(() => {
 
-      Alert.alert(
-        "You are not verified to use this feature.",
-        "Become An Expert /Buisness Parthners after Submiting Your Simple and Easy KYC form and will let you know... and you after some decision. and verification.",
-        [
-          {
-            text: "Cancel",
-            onPress: () => {
-              // Redirect to another page here
-              navigation.navigate('profile'); // Navigate to AnotherPage
-            }
-          },
-          {
-            text: "Go",
-            onPress: () => {
-              // Redirect to another page here
-              navigation.navigate('expertverify'); // Navigate to AnotherPage
-            }
-          }
-        ]
-      );
-
-
-
-    }, [navigator])
-  );
   return (
     <View style={style.container}>
-    
+
     </View>
   )
 }
 const style = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:"#fff"
+    backgroundColor: "#fff"
   }
 })
 export default ExpertHailingPage

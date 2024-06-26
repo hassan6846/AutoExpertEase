a77ed1e34266c84ef54b9fd78ad475ee7e611d1c
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-06-27 01:00:09 +0500

    Update VendorVerfication.tsx

--- app/screens/ExpertVerification/nested/ExpertVerification/VendorVerfication.tsx
+++ app/screens/ExpertVerification/nested/ExpertVerification/VendorVerfication.tsx
@@ -12,7 +12,7 @@ const VendorVerfication = () => {
     const [email, setEmail] = useState("")
     const [phone, setphone] = useState("")
     const [address, setAddress] = useState("")
-    const [coordinates,setcoordinates] = useState("")
+    const [coordinates,setcoordinates] = useState("-35.3,-40.2")
     const [accountno, setAccountno] = useState("")
     const [ntnno, setNtnno] = useState("")
     const [cnic, setCnic] = useState("")
@@ -41,16 +41,16 @@ const handleFormSubmit = () => {
             </View>
             <View style={{ backgroundColor: "#fff", borderRadius: 5, padding: 10, marginBottom: 10 }}>
                 <Text style={{ textAlign: "center" }} h4>Payment Information </Text>
-                <InputComponent placeholder="Enter Account Number to recieve payments." label="Country" />
+                <InputComponent value={accountno} onChangeText={setAccountno} placeholder="Enter Account Number to recieve payments." label="AccNo." />
 
 
             </View>
 
             <View style={{ backgroundColor: "#fff", borderRadius: 5, padding: 10, marginBottom: 50 }}>
                 <Text style={{ textAlign: "center" }} h4>Document Details </Text>
-                <InputComponent placeholder="Enter Cnic Numnber" label="cnic no" />
-                <InputComponent placeholder="Enter ntn number" label="ntn" />
-                <CustomButton function={handleFormSubmit} title="Request Vendor Ship"/>
+                <InputComponent  value={cnic} onChangeText={setCnic} placeholder="Enter Cnic Numnber" label="cnic no" />
+                <InputComponent value={ntnno} onChangeText={setNtnno} placeholder="Enter ntn number" label="ntn" />
+                <CustomButton  function={handleFormSubmit} title="Request Vendor Ship"/>
             </View>
           
         </ScrollView>

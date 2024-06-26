9e2727d8e330de414ff45102c85011004b516342
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-06-27 00:57:19 +0500

    main

--- app/screens/ExpertVerification/nested/ExpertVerification/VendorVerfication.tsx
+++ app/screens/ExpertVerification/nested/ExpertVerification/VendorVerfication.tsx
@@ -1,5 +1,5 @@
 import { ScrollView, StyleSheet, View } from 'react-native'
-import React from 'react'
+import React,{useState} from 'react'
 
 //librarry 
 import { Text } from "@rneui/themed"
@@ -8,23 +8,35 @@ import InputComponent from '../../../../components/InputComponent/InputComponent
 import CustomButton from '../../../../components/ButtonProps/ButtonProps'
 
 const VendorVerfication = () => {
+    const [shopname, setShopname] = useState("")
+    const [email, setEmail] = useState("")
+    const [phone, setphone] = useState("")
+    const [address, setAddress] = useState("")
+    const [coordinates,setcoordinates] = useState("")
+    const [accountno, setAccountno] = useState("")
+    const [ntnno, setNtnno] = useState("")
+    const [cnic, setCnic] = useState("")
+const handleFormSubmit = () => {
+    // handle form submit
+    console.log({ shopname, email, phone, address, coordinates, accountno, ntnno, cnic })
+}
     return (
         <ScrollView style={styles.container}>
             <View style={{ backgroundColor: "#fff", borderRadius: 5, padding: 10, marginBottom: 10 }}>
                 <Text style={{ textAlign: "center" }} h4>Basic Info</Text>
-                <InputComponent label="Shop Name" />
+                <InputComponent value={shopname}  onChangeText={setShopname} label="Shop Name" />
 
-                <InputComponent placeholder="Use active email to recieve messsages and alerts" label="E-mail" />
-                <InputComponent label="Buisness Phone" />
+                <InputComponent value={email} onChangeText={setEmail} placeholder="Use active email to recieve messsages and alerts" label="E-mail" />
+                <InputComponent value={phone} onChangeText={setphone} label="Buisness Phone" />
             </View>
 
             <View style={{ backgroundColor: "#fff", borderRadius: 5, padding: 10, marginBottom: 10 }}>
                 <Text style={{ textAlign: "center" }} h4>Buisness Details</Text>
                 <InputComponent value="Pakistan" label="Country" />
                 <InputComponent value="Lahore" label="city" />
-                <InputComponent label="Address/Pickup Location" />
+                <InputComponent value={address} onChangeText={setAddress} label="Address/Pickup Location" />
                 <InputComponent value="54000" label="ZipCode /Postal Code" />
-                <InputComponent placeholder="Helps to Find you nearby Customers for faster Sales (only Signup this form at buisness place ) " label="Buisness Coordinates" />
+                <InputComponent value={coordinates} onChangeText={setcoordinates} placeholder="Helps to Find you nearby Customers for faster Sales (only Signup this form at buisness place ) " label="Buisness Coordinates" />
 
             </View>
             <View style={{ backgroundColor: "#fff", borderRadius: 5, padding: 10, marginBottom: 10 }}>
@@ -38,7 +50,7 @@ const VendorVerfication = () => {
                 <Text style={{ textAlign: "center" }} h4>Document Details </Text>
                 <InputComponent placeholder="Enter Cnic Numnber" label="cnic no" />
                 <InputComponent placeholder="Enter ntn number" label="ntn" />
-                <CustomButton title="Request Vendor Ship"/>
+                <CustomButton function={handleFormSubmit} title="Request Vendor Ship"/>
             </View>
           
         </ScrollView>

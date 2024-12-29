c0433b7f4d2b4494473f0484f7fa42d164848641
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-12-29 12:49:56 +0500

    Implement serverless functionality and update package dependencies

--- app/server/app.js
+++ app/server/app.js
@@ -16,20 +16,20 @@ require("dotenv").config();
 // Middlewares
 app.disable("x-powered-by"); // Hide tech stack from hackers
 app.use(fileupload()); // Use fileupload middleware
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
 app.use(express.json()); // Server is JSON type
 app.use(cookieParser());
 app.use(
   bodyParser.json({
     limit: "100mb",
-  
+
   })
 );
 app.use(
   bodyParser.urlencoded({
     limit: "100mb",
     extended: true,
-    parameterLimit:50000000000,
+    parameterLimit: 50000000000,
     type: "application/json"
   })
 );
@@ -41,12 +41,12 @@ const chatbot = require("./routes/Chatbot");
 const admin = require("./routes/AdminRoutes");
 const auth = require("./routes/AuthRoutes");
 const payment = require("./routes/PaymentRoutes");
-const car=require("./routes/CarRoutes")
-const expert=require("./routes/ExpertRoute")
-const location=require("./routes/LocationRoutes")
-const order=require("./routes/OrderRoutes")
-const vendor=require("./routes/VendorRoutes")
-const booking=require("./routes/BookingRoute")
+const car = require("./routes/CarRoutes")
+const expert = require("./routes/ExpertRoute")
+const location = require("./routes/LocationRoutes")
+const order = require("./routes/OrderRoutes")
+const vendor = require("./routes/VendorRoutes")
+const booking = require("./routes/BookingRoute")
 // Endpoints middlewares
 app.use("/api", user);
 app.use("/api", product);
@@ -54,12 +54,12 @@ app.use("/api", chatbot);
 app.use("/api", admin);
 app.use("/api", auth);
 app.use("/api", payment);
-app.use("/api",car);
-app.use("/api",expert);
-app.use("/api",location);
-app.use('/api',order);
-app.use('/api',vendor);
-app.use('/api',booking);
+app.use("/api", car);
+app.use("/api", expert);
+app.use("/api", location);
+app.use('/api', order);
+app.use('/api', vendor);
+app.use('/api', booking);
 // Create HTTP server
 const server = http.createServer(app);
 
@@ -70,7 +70,7 @@ ConnectMongodb();
 
 // Listen for both HTTP and WebSocket connections
 const Port = process.env.PORT || 3000;
-server.listen(Port,'0.0.0.0', () => {
+server.listen(Port, '0.0.0.0', () => {
   console.log(`App is running on Port ${Port}`);
 });
 
@@ -79,3 +79,5 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Something broke!");
 });
+
+module.exports = app;
